7cd15073e59487dd148205a286a3f01e447e8418
onx66 <oipek01@mail.com>
2026-01-14 12:04:06 +0300

    onur-try-this

---
+++ mxi-design/src/assets/svg/AboutUsIcon.jsx
@@ -0,0 +1,18 @@
+const AboutUsIcon = ({ size = 48 }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 256 256"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={16}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <circle cx="128" cy="96" r="64" />
+    <path d="M30.989 215.991a112.037 112.037 0 0 1 194.023.002" />
+  </svg>
+);
+
+export default AboutUsIcon;
--- mxi-design/src/components/Header/Header.jsx
+++ mxi-design/src/components/Header/Header.jsx
@@ -2,15 +2,24 @@
 
 import ContactUsButton from "../ContactUsButton/ContactUsButton";
 import ProductsButton from "../ProductsButton/ProductsButton";
+import Radio from "../Radio/Radio";
+import { useWindowWidth } from "../../hooks/useWindowWidth";
 import "./Header.css";
 
-
-
 function Header() {
+    const width = useWindowWidth();
+
     return (
-            <header >
-                <div className="header-container">
-                    <img className="logo" src="https://mxi-design.com/wp-content/uploads/2025/03/mxi_sitelogo.png" />
+        <header>
+            <div className="header-container">
+                <img
+                    className="logo"
+                    src="https://mxi-design.com/wp-content/uploads/2025/03/mxi_sitelogo.png"
+                />
+
+                {width < 600 ? (
+                    <Radio />
+                ) : (
                     <ul className="menu">
                         <li>Home</li>
                         <li><ProductsButton /></li>
@@ -18,12 +27,14 @@ function Header() {
                         <li>Partners</li>
                         <li>About Us</li>
                     </ul>
-                </div>
-                <div className="contact-us-button">
-                    <ContactUsButton />
-                </div>
-            </header>
-    )
+                )}
+            </div>
+
+            <div className="contact-us-button">
+                <ContactUsButton />
+            </div>
+        </header>
+    );
 }
 
 export default Header;
