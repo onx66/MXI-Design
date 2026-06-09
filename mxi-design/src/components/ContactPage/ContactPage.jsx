ca18849ffd23f5230c2e503fa02d45b30a0d4669
onx66 <oipek01@mail.com>
2026-06-09 13:45:21 +0300

    onur-price-done

--- mxi-design/src/components/ContactPage/ContactPage.jsx
+++ mxi-design/src/components/ContactPage/ContactPage.jsx
@@ -123,7 +123,7 @@ function ContactPage() {
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                                     <path d="M20 6L9 17l-5-5" />
                                 </svg>
-                                <span>Your message has been sent successfully!</span>
+                                <span>Your message has been received successfully!</span>
                             </div>
                         )}
 
